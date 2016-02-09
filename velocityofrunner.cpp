1daa99221b9ce33c390b583daea5f77ace721786
james wimberly <wimberlyj@uindy.edu>
2016-02-09 17:29:04 -0500

    first commit

---
+++ velocityofrunner.cpp
@@ -0,0 +1,23 @@
+// This progam calculates the user velocity. 
+#include <iostream>
+using namespace std;
+ 
+int main() { 
+//setting up variables
+double meters , seconds, velocity; 
+ 
+// Get the amount of meters ran. 
+cout << "How many meters did the runner run? "; 
+cin >> meters; 
+ 
+// Get the seconds the runner ran. 
+cout << "How many seconds did runner run? "; 
+cin >> seconds; 
+ 
+// Calculate the velocity. 
+velocity = meters / second; 
+ 
+// Display the velocity. 
+cout << "You ran at meters/second" << velocity << endl; 
+return 0; 
+}
